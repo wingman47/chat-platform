40f208c66fe0b527d9768339e1376873d8bea38b
Anand <anotheranand@gmail.com>
2023-10-03 23:56:35 +0530

    project initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route } from "react-router-dom";
+import "./App.css";
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Route path="/" Component={} />
+      <Route path="/chats" /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { chats } from "./data/data.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("api is runnning");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const { id } = req.params;
+  const chat = chats.find((c) => c._id === id);
+  res.send(chat);
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, console.log(`server started at ${PORT}`));

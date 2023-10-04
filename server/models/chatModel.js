548fcfda1b4b9fba53346a5688fa1a4d2e61964b
Anand <anotheranand@gmail.com>
2023-10-04 23:18:38 +0530

    added db models

---
+++ server/models/chatModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroup: { type: Boolean, dfault: false },
+    users: [
+      {
+        // each user in the array is an objectId values which references to
+        // documents in other "User" collection
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+module.exports = Chat

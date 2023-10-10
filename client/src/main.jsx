2349cd4018a48c365d7a2a52ce5a06e63a53b084
Arpit Anand <arpitanandnova@gmail.com>
2023-10-10 15:31:02 +0530

    added redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,49 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import authReducer from "./state/state";
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-);
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </PersistGate>
+  </Provider>
+)
